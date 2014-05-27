4f49f2b15491354e208b9e7839dcff033d5de4b7
David Boivin ~Spit <db2_hockey@hotmail.com>
2014-05-27 02:13:05 -0400

    Initial Commit

---
+++ ExpressionParser/src/ExpressionParser.java
@@ -0,0 +1,12 @@
+
+
+
+/**
+ * This class contains different methods to allow a mathematical expression
+ * to be parsed and evaluated.
+ *
+ * @author David Boivin
+ */
+public class ExpressionParser {
+	//TODO implement (more additives)
+}
---
+++ ExpressionParser/src/ExpressionParserDemo.java
@@ -0,0 +1,16 @@
+
+
+
+/**
+ * This class is a demo to show case the expression parser.
+ *
+ * @author David Boivin
+ */
+public class ExpressionParserDemo {
+
+	public static void main(String[] args) {
+		// TODO write very important part of the testing of this program (Some additives)
+		
+	}
+
+}
