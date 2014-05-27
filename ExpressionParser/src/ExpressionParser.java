692e9b68a34a038f0da20e5392b76d0178133565
David Boivin ~Spit <db2_hockey@hotmail.com>
2014-05-27 02:22:54 -0400

    Created the public parse and evaluate methods

--- ExpressionParser/src/ExpressionParser.java
+++ ExpressionParser/src/ExpressionParser.java
@@ -1,6 +1,8 @@
 
 
 
+import java.util.ArrayDeque;	//used as a stack
+
 /**
  * This class contains different methods to allow a mathematical expression
  * to be parsed and evaluated.
@@ -8,5 +10,13 @@
  * @author David Boivin
  */
 public class ExpressionParser {
-	//TODO implement (more additives)
+	
+	public static ArrayDeque<String> parse(String expresson){
+		return null;	//TODO implement
+	}
+	
+	public static double evaluate(String expression){
+		return -1;		//TODO implement 
+	}
+	
 }
