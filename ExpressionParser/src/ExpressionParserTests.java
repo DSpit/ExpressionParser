a20e864f8a12885fce6fd48b6fa1dacb8db36784
David Boivin ~Spit <db2_hockey@hotmial.com>
2014-05-30 12:59:45 -0400

    -add bracket tests -add general expression tests -add error cases to testing (not implemented yet)

--- ExpressionParser/src/ExpressionParserTests.java
+++ ExpressionParser/src/ExpressionParserTests.java
@@ -164,7 +164,80 @@ public class ExpressionParserTests {
 		System.out.println("\t\t\t\t\tSuccess: " + e.equals(r));
 		System.out.println("\n");
 		
-		//TODO need to add bracket tests and general long expressions tests
+		//testing brackets within the expression
+		System.out.println("20. (6-2)/4");
+		e = "[6.0, 2.0, -, 4.0, /]";
+		r = ExpressionParser.parse("(6-2)/4").toString();
+		System.out.println("Expected: " + e);
+		System.out.println("Result: " + r);
+		System.out.println("\t\t\t\t\tSuccess: " + e.equals(r));
+		System.out.println("\n");
+		
+		System.out.println("20. 4/(6-2)");
+		e = "[4.0, 6.0, 2.0, -, /]";
+		r = ExpressionParser.parse("4/(6-2)").toString();
+		System.out.println("Expected: " + e);
+		System.out.println("Result: " + r);
+		System.out.println("\t\t\t\t\tSuccess: " + e.equals(r));
+		System.out.println("\n");
+		
+		System.out.println("21. (6+6)/(6-2)");
+		e = "[6.0, 6.0, +, 6.0, 2.0, -, /]";
+		r = ExpressionParser.parse("(6+6)/(6-2)").toString();
+		System.out.println("Expected: " + e);
+		System.out.println("Result: " + r);
+		System.out.println("\t\t\t\t\tSuccess: " + e.equals(r));
+		System.out.println("\n");
+		
+		System.out.println("22. ((6-2))");
+		e = "[6.0, 2.0, -]";
+		r = ExpressionParser.parse("((6-2))").toString();
+		System.out.println("Expected: " + e);
+		System.out.println("Result: " + r);
+		System.out.println("\t\t\t\t\tSuccess: " + e.equals(r));
+		System.out.println("\n");
+		
+		//testing general long expressions
+		System.out.println("23. 2+3*32/64^(1/2)-18*2");
+		e = "[2.0, 3.0, 32.0, 64.0, 1.0, 2.0, /, ^, /, +, 18.0, 2.0, *, -]";
+		r = ExpressionParser.parse("2+3*32/64^(1/2)-18*2").toString();
+		System.out.println("Expected: " + e);
+		System.out.println("Result: " + r);
+		System.out.println("\t\t\t\t\tSuccess: " + e.equals(r));
+		System.out.println("\n");
+		
+		System.out.println("24. ((3*(12+2-8))/((24+6)/10))^(1/3)");
+		e = "[3.0, 12.0, 2.0, +, 8.0, -, *, 24.0, 6.0, +, 10.0, /, /, 1.0, 3.0, /, ^]";
+		r = ExpressionParser.parse("(3*(12+2-8))/((24+6)/10)^(1/3)").toString();
+		System.out.println("Expected: " + e);
+		System.out.println("Result: " + r);
+		System.out.println("\t\t\t\t\tSuccess: " + e.equals(r));
+		System.out.println("\n");
+		
+		//testing error handling TODO
+		System.out.println("25. 2 2+8");
+		System.out.println("Not implemented yet");
+		System.out.println("\n");
+		
+		System.out.println("26. 2+8+");
+		System.out.println("Not implemented yet");
+		System.out.println("\n");
+		
+		System.out.println("27. (2+4");
+		System.out.println("Not implemented yet");
+		System.out.println("\n");
+		
+		System.out.println("28. 2+4)");
+		System.out.println("Not implemented yet");
+		System.out.println("\n");
+		
+		System.out.println("29. 2 2+");
+		System.out.println("Not implemented yet");
+		System.out.println("\n");
+		
+		System.out.println("30. 2++4");
+		System.out.println("Not implemented yet");
+		System.out.println("\n");
 		
 		//System.out.println("Testing evaluate() method");
 	}
